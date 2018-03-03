45ae7cd6f0a41fcfc6285063142f7b852a027e94
Your Name <landonmarshall@w134-87-181-099.wireless.uvic.ca>
2018-03-02 23:31:13 -0800

    snake can find best route

--- app/main.py
+++ app/main.py
@@ -41,15 +41,25 @@ def shortest_path(snake, food):
         distance.append((food[i],abs(food[i][0]-snake[0])+abs(food[i][1]-snake[1])))
     return sorted(distance, key=lambda distance: distance[1])
 
-def goto(snake, coordinate, danger):
-    if(snake[0]-food[0][0] > 0 && danger[0] not 1):
+def goto(snake, food, danger):
+    max_pos = max(danger)
+    index = 0
+    for i in range(len(danger)):
+        if danger[i] == max_pos:
+            index = i
+    directions = ['left',  'right', 'up', 'down']
+    print snake,food,danger
+    if(snake[0]-food[0] > 0 and danger[0] > 1):
         return "left"
-    elif(snake[0]-food[0][0] < 0 && danger[1] not 1):
+    elif(snake[0]-food[0] < 0 and danger[1] > 1):
         return "right"
-    elif(snake[1]-food[1] > 0 && danger[2] not 1):
+    elif(snake[1]-food[1] > 0 and danger[2] > 1):
         return "up"
-    elif(snake[1]-food[1] < 0 && danger[3] not 1):
+    elif(snake[1]-food[1] < 0 and danger[3] > 1):
         return "down"
+    else:
+        return directions[index]
+
 '''
 co-ords:
     print data['snakes']['data'][0]['body']['data'][0]['x']
@@ -86,7 +96,6 @@ def start():
         'head_url': head_url
     }
 
-
 @bottle.post('/move')
 def move():
     data = bottle.request.json
@@ -97,53 +106,63 @@ def move():
     snakekey = find_us(data)
     print 'our sneks key: ', snakekey
     oursnake_head, food_pos = find_positions(data)
-    print 'Danger List: ', danger(data)
-    danger_list = danger(data)
-    moves = safemoves(oursnake_head,danger_list)
+    print 'Danger List: ', danger(data, oursnake_head)
+    danger_list = danger(data, oursnake_head)
+    moves = dangerdistance(oursnake_head, danger_list)
     print moves
-    #print shortest_path(snake_pos, food_pos)
+    closest_food = shortest_path(oursnake_head, food_pos)
     ourlength = data.get('snakes').get('data')[snakekey].get('length')
-   # print data['snakes']['data'][0]['body']['data'][0]['x']
-   # print data['snakes']['data'][0]['body']['data'][0]['y']
     directions = ['left',  'right', 'up', 'down']
-    safe_moves = []
-    for i in range(len(moves)):
-        if moves[i] == 0:
-            safe_moves.append(i)
-    print safe_moves
-    print random.choice(safe_moves)
-    direction = directions[random.choice(safe_moves)]
+    direction = goto(oursnake_head,closest_food[0][0],moves)
 
     return {
         'move': direction,
         'taunt': 'Bill! Bill! Bill! Bill!'
     }
 
-def safemoves(oursnake_head, danger_list):
-    #left,right,up,down
+def dangerdistance(oursnake_head, danger_list):
     moves = [0] * 4
-    left = (oursnake_head[0]-1,oursnake_head[1])
-    right = (oursnake_head[0]+1,oursnake_head[1])
-    up = (oursnake_head[0],oursnake_head[1]-1)
-    down = (oursnake_head[0],oursnake_head[1]+1)
-    if left in danger_list:
-        moves[0] = 1
-    if right in danger_list:
-        moves[1] = 1
-    if up in danger_list:
-        moves[2] = 1
-    if down in danger_list:
-        moves[3] = 1
+    headx = oursnake_head[0]
+    heady = oursnake_head[1]
+    leftdist = []
+    rightdist = []
+    updist = []
+    downdist = []
+    for i in range(len(danger_list)):
+        
+        if headx == danger_list[i][0]:
+            print 'headx ', headx
+            print 'danger_list[i][1] ', danger_list[i][1]
+            if heady > danger_list[i][1]:
+                updist.append(heady - danger_list[i][1])
+            else:
+                downdist.append(danger_list[i][1] - heady)
+        if heady == danger_list[i][1]:
+            print 'heady ', heady
+            print 'danger_list[i][0] ', danger_list[i][0]
+            if headx > danger_list[i][0]:
+                leftdist.append(headx -danger_list[i][0])
+            else:
+                rightdist.append(danger_list[i][0] - headx)
+    print 'leftdist ',leftdist
+    print 'rightdist ', rightdist
+    print 'updist ', updist
+    print 'downdist ', downdist
+    moves[0] = min(leftdist)
+    moves[1] = min(rightdist)
+    moves[2] = min(updist)
+    moves[3] = min(downdist)
+    print moves
     return moves
 
 
-
 #This function returns a list of every snake body item as co-ordinates
-def danger(data):
+def danger(data, oursnake_head):
     danger_list = []
     for i in range(len(data['snakes']['data'])):
             for k in range(data['snakes']['data'][i]['length']):
-                danger_list.append((data['snakes']['data'][i]['body']['data'][(k)]['x'],data['snakes']['data'][i]['body']['data'][(k)]['y']))
+                if (oursnake_head != (data['snakes']['data'][i]['body']['data'][(k)]['x'],data['snakes']['data'][i]['body']['data'][(k)]['y'])):
+                    danger_list.append((data['snakes']['data'][i]['body']['data'][(k)]['x'],data['snakes']['data'][i]['body']['data'][(k)]['y']))
     for i in range(board_height):
         danger_list.append((-1,i))
         danger_list.append((board_width,i))
@@ -155,8 +174,6 @@ def danger(data):
         #right wall = board_width, y
         #bottom wall = x, board_height
     return danger_list
-
-
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
