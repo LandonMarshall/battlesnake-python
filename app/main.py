2e29528b9bd195c7d66b433bff8a84218ec56623
Your Name <landonmarshall@Lauries-MacBook-Pro.local>
2018-03-03 13:01:57 -0800

    added head and tail type

--- app/main.py
+++ app/main.py
@@ -94,7 +94,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#FF0000',
+        'color': '##00d635',
         "secondary_color": "#00FF00",
         "head_type": "smile",
         "tail_type": "fat-rattle",
